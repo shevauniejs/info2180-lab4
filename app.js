ac4e6037a84cd2847ce4a4502db3474260665524
Shevaunie Senior <shevaunie.senior@mymona.uwi.edu>
2025-11-20 15:16:09 -0500

    results when button clicked redone w/ jquery ajax

--- app.js
+++ app.js
@@ -2,25 +2,14 @@ document.addEventListener('DOMContentLoaded',function(){
     console.log("Loaded");
     let btn = document.getElementById("charSub");
     
-    
+
     let btnFunc = function(){
-        console.log("Button clicked");
-        if(httpRequest.readyState===XMLHttpRequest.DONE){
-            if(httpRequest.status===200){
-                let response = httpRequest.responseText;
-                alert(response);
-            }else{
-                alert("There was a problem with the request.");
-            }
-        }
+        $.ajax("superheroes.php")
+            .done(function(result){
+                alert(result)}).fail(function(result){
+                    alert("No");
+            });
     }
-    
-    const httpRequest = new XMLHttpRequest();
-    let url = "superheroes.php";
-    httpRequest.onreadystatechange = btnFunc;
-    httpRequest.open('GET',url);
-    httpRequest.send();
-    
     
     btn.addEventListener("click", btnFunc);
 
